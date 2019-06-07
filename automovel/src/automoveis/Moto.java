7478fd4f070ac66b739812f93ae88336af942d95
Fábio Vito <fabiovito@hotmail.com>
2019-06-07 07:57:32 -0300

    add Moto.construtores

---
+++ automovel/src/automoveis/Moto.java
@@ -0,0 +1,81 @@
+package automoveis;
+
+import motor.Motor;
+
+public class Moto extends Automovel{
+    private int volumeBagageiro;
+    
+    public void setVolumeBagageiro(int bagageiro){
+        volumeBagageiro = bagageiro;
+    }
+    public int getVolumeBagageiro(){
+        return volumeBagageiro;
+    }    
+    
+    // CONSTRUTORES:
+    public Moto(){}
+    public Moto(String marca){
+        setMarca(marca);        
+    }
+    public Moto(int bagageiro){
+       setVolumeBagageiro(bagageiro);
+    }
+    public Moto(Motor motor, String proprietario, String marca,
+            String placa, int passageiros, double preco, int bagageiro){
+        setMotor(motor);
+        setProprietario(proprietario);
+        setMarca(marca);
+        setPlaca(placa);
+        setNumeroPassageiros(passageiros);
+        setPreco(preco);
+        setVolumeBagageiro(bagageiro);        
+    }
+    public Moto(String proprietario, String marca,String placa, 
+            int passageiros, double preco, int bagageiro){
+        setProprietario(proprietario);
+        setMarca(marca);
+        setPlaca(placa);
+        setNumeroPassageiros(passageiros);
+        setPreco(preco);
+        setVolumeBagageiro(bagageiro);        
+    }
+    public Moto(String marca,String placa, int passageiros, 
+            double preco, int bagageiro){
+        setMarca(marca);
+        setPlaca(placa);
+        setNumeroPassageiros(passageiros);
+        setPreco(preco);
+        setVolumeBagageiro(bagageiro);        
+    }
+    public Moto(String placa, int passageiros, double preco, int bagageiro){
+        setPlaca(placa);
+        setNumeroPassageiros(passageiros);
+        setPreco(preco);
+        setVolumeBagageiro(bagageiro);        
+    }
+    public Moto(Motor motor, String marca,String placa, 
+            int passageiros, double preco, int bagageiro){
+        setMotor(motor);
+        setMarca(marca);
+        setPlaca(placa);
+        setNumeroPassageiros(passageiros);
+        setPreco(preco);
+        setVolumeBagageiro(bagageiro);        
+    }
+    public Moto(Motor motor, String proprietario, String marca,
+            double preco, int bagageiro){
+        setMotor(motor);
+        setProprietario(proprietario);
+        setMarca(marca);
+        setPreco(preco);
+        setVolumeBagageiro(bagageiro);        
+    }
+    public Moto(Motor motor, String proprietario, String marca,
+            String placa){
+        setMotor(motor);
+        setProprietario(proprietario);
+        setMarca(marca);
+        setPlaca(placa);      
+    }
+    
+}
