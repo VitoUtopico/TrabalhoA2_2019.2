fb73980010a137e48c732178810b588643767fde
Fábio Vito <fabiovito@hotmail.com>
2019-06-07 08:00:56 -0300

    add Onibus.construtores

---
+++ automovel/src/automoveis/Onibus.java
@@ -0,0 +1,84 @@
+package automoveis;
+import motor.Motor;
+public class Onibus extends Automovel{
+    private String motorista;
+    
+    // SETTER:
+    public void setMotorista(String motorista){
+        this.motorista = motorista;
+    }
+    // GETTER:
+    public String getMotorista(){
+        return motorista;
+    }
+    
+    // CONSTRUTORES:
+    public Onibus(){}
+    public Onibus(String marca){
+        setMarca(marca);        
+    }
+    public Onibus(String motorista, String marca){
+       setMotorista(motorista);
+       setMarca(marca);
+    }
+    public Onibus(Motor motor, String proprietario, String marca,
+            String placa, int passageiros, double preco, String motorista){
+        setMotor(motor);
+        setProprietario(proprietario);
+        setMarca(marca);
+        setPlaca(placa);
+        setNumeroPassageiros(passageiros);
+        setPreco(preco);
+        setMotorista(motorista);        
+    }
+    public Onibus(String proprietario, String marca,String placa, 
+            int passageiros, double preco, String motorista){
+        setProprietario(proprietario);
+        setMarca(marca);
+        setPlaca(placa);
+        setNumeroPassageiros(passageiros);
+        setPreco(preco);
+        setMotorista(motorista);        
+    }
+    public Onibus(String marca,String placa, int passageiros, 
+            double preco, String motorista){
+        setMarca(marca);
+        setPlaca(placa);
+        setNumeroPassageiros(passageiros);
+        setPreco(preco);
+        setMotorista(motorista);        
+    }
+    public Onibus(String placa, int passageiros, double preco, String motorista){
+        setPlaca(placa);
+        setNumeroPassageiros(passageiros);
+        setPreco(preco);
+        setMotorista(motorista);        
+    }
+    public Onibus(Motor motor, String marca,String placa, 
+            int passageiros, double preco, String motorista){
+        setMotor(motor);
+        setMarca(marca);
+        setPlaca(placa);
+        setNumeroPassageiros(passageiros);
+        setPreco(preco);
+        setMotorista(motorista);        
+    }
+    public Onibus(Motor motor, String proprietario, String marca,
+            double preco, String motorista){
+        setMotor(motor);
+        setProprietario(proprietario);
+        setMarca(marca);
+        setPreco(preco);
+        setMotorista(motorista);        
+    }
+    public Onibus(Motor motor, String proprietario, String marca,
+            String placa){
+        setMotor(motor);
+        setProprietario(proprietario);
+        setMarca(marca);
+        setPlaca(placa);      
+    }
+
+    
+    
+}
