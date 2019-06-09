c1e9b0f2a0791133520bf77ef23c051fb6a49b6a
Fábio Vito <fabiovito@hotmail.com>
2019-06-09 12:20:41 -0300

    Add classe abstrata InterfaceAutomoveis()

---
+++ automovel/src/abstratas/InterfaceAutomoveis.java
@@ -0,0 +1,10 @@
+package abstratas;
+public interface InterfaceAutomoveis {
+    
+    public abstract int acelerar();
+    public abstract int desacelerar();
+    public abstract String frear();
+    public abstract String virarAEsquerda();
+    public abstract String virarSDireita();
+    
+}
